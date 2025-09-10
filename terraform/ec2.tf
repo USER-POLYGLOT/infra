6c09fd4fa3f8bc2be7f4ae2de1a04b22c5623e64
anubhav1404 <116413657+anubhav1404@users.noreply.github.com>
2025-09-10 11:49:11 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "test_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  user_data     = templatefile("${path.module}/user_setup.sh", { runner_token = var.runner_token })
+
+  tags = {
+    Name = "Anubhav-instance"
+  }
+}
