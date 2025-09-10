5e5b36eb5b4a5e4e95eb0ca44cc91c354f44eeb0
anubhav1404 <116413657+anubhav1404@users.noreply.github.com>
2025-09-10 10:42:44 +0000

    Added

--- terraform/user_setup.sh
+++ terraform/user_setup.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
-set -e  # Exit on error
-
-# Update and install prerequisites
 sudo apt-get update -y
 sudo apt-get install -y curl tar jq
 
-# Create runner directory
-mkdir -p ~/actions-runner
-cd ~/actions-runner
-
-# Download and extract runner
+# Create runner dir
+sudo mkdir -p /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner
 curl -o actions-runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
-tar xzf actions-runner.tar.gz
+tar xzf ./actions-runner.tar.gz
 rm actions-runner.tar.gz
 
-# Configure runner (make sure runner_token is exported)
-./config.sh --unattended \
+sudo chown -R ubuntu:ubuntu /home/ubuntu/actions-runner
+
+sudo -u ubuntu ./config.sh --unattended \
   --url https://github.com/USER-POLYGLOT \
   --token "${runner_token}" \
   --name ec2-org-runner \
   --labels self-hosted,linux,ec2 \
   --work "_work"
 
-# Install and start the service
+
+
+# Install service
 sudo ./svc.sh install
 sudo ./svc.sh start
-
-echo "Runner installed and started!"
