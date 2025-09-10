1de530a454a950cf96de8f244c8c37ab68ed55f2
anubhav1404 <116413657+anubhav1404@users.noreply.github.com>
2025-09-10 12:09:45 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = "anubhavnv"
 
   user_data     = templatefile("${path.module}/user_setup.sh", { runner_token = var.runner_token })
 
