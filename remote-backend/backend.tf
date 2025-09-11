56094d9d72244adff86387da07b99a07467f2bc0
anubhav1404 <116413657+anubhav1404@users.noreply.github.com>
2025-09-11 10:00:33 +0530

    Update backend.tf

--- remote-backend/backend.tf
+++ remote-backend/backend.tf
@@ -2,14 +2,14 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "user-polyglot-terraform-state"   # must be unique across AWS
 }
 
-resource "aws_s3_bucket_versioning" "enabled" {
+resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
   rule {
