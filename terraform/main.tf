91925eb103796007f55169d29e552fd05d3fab09
anubhav1404 <116413657+anubhav1404@users.noreply.github.com>
2025-09-11 09:37:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,19 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "user-polyglot-terraform-state"   # must be globally unique in AWS
+    key            = "k8s-cluster/terraform.tfstate"   # state file path inside the bucket
+    region         = "us-east-1"                       # adjust if needed
+    dynamodb_table = "terraform-locks"                 # for state locking
+    encrypt        = true
   }
 }
 
